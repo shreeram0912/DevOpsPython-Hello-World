3e930a597c2723f08286f93bd79454634581709f
shreeram0912 <sprajapati99.sp@gmail.com>
2025-03-24 13:08:48 +0530

    Added hello.py to the repository

--- hello.py
+++ hello.py
@@ -1,6 +1,1 @@
-def greet_user():
-    name = input("Please enter your name: ")  # Get the user's name
-    print(f"Hello, {name}!")  # Print a dynamic greeting
-
-# Call the greet_user function
-greet_user()
+print("Hello, World!")
